11a4e19a88bde2a112f304b09183fd7cb544a856
krishd9895 <105974502+krishd9895@users.noreply.github.com>
2024-06-17 19:55:27 +0530

    Update constant.py

--- ytdlbot/constant.py
+++ ytdlbot/constant.py
@@ -23,35 +23,25 @@ from utils import get_func_queue
 
 class BotText:
     start = """
-    Welcome to YouTube Download bot. Type /help for more information. Recommend to use EU Bot
-    EU🇪🇺: @benny_2ytdlbot
-    SG🇸🇬：@benny_ytdlbot
-
-    Join https://t.me/+OGRC8tp9-U9mZDZl for updates."""
+    Welcome to YouTube Download bot. Type /help for more information.
+    
+    """
 
     help = """
 1. For YouTube and any websites supported by yt-dlp, just send the link and we will download and send it to you.
 
-2. For specific links use `/spdl {URL}`. More info at https://github.com/SanujaNS/ytdlbot-telegram#supported-websites 
+2. For specific links use `/spdl {URL}`.  
 
-3. If the bot doesn't work, try again or join https://t.me/+OGRC8tp9-U9mZDZl for updates.
-
-4. Wanna deploy it yourself?\nHere's the source code: https://github.com/tgbot-collection/ytdlbot
     """
 
-    about = "YouTube Downloader by @BennyThink.\n\nOpen source on GitHub: https://github.com/tgbot-collection/ytdlbot"
+    about = "YouTube Downloader"
 
     buy = f"""
 **Terms:**
 1. You can use this bot to download video for {FREE_DOWNLOAD} times within a 24-hour period.
 
-2. You can buy additional download tokens, valid permanently.
 
-3. Refunds are possible, contact me if you need that @BennyThink
 
-4. Download for paid user will be automatically changed to Local mode to avoid queuing.
-
-5. Paid user can download files larger than 2GB.
 
 **Price:**
 valid permanently
@@ -61,22 +51,17 @@ valid permanently
 
 **Payment options:**
 Pay any amount you want. For example you can send 20 TRX for {TOKEN_PRICE * 2} tokens.
-1. AFDIAN(AliPay, WeChat Pay and PayPal): {AFD_LINK}
-2. Buy me a coffee: {COFFEE_LINK}
-3. Telegram Bot Payment(Stripe), please click Bot Payment button.
-4. TRON(TRX), please click TRON(TRX) button.
+
 
 **After payment:**
-1. Afdian: attach order number with /redeem command (e.g., `/redeem 123456`).
-2. Buy Me a Coffee: attach email with /redeem command (e.g., `/redeem 123@x.com`). **Use different email each time.**
-3. Telegram Payment & Tron(TRX): automatically activated within 60s. Check /start to see your balance.
 
-Want to buy more token with Telegram payment? Let's say 100? Here you go! `/buy 123`
+
+Want to buy more token with Telegram payment? Let's say 100? Here you go! 
     """
 
     private = "This bot is for private use"
 
-    membership_require = f"You need to join this group or channel to use this bot\n\nhttps://t.me/{REQUIRED_MEMBERSHIP}"
+    membership_require = f"You need to join this group or channel to use this bot\n\nhttps://t.me/"
 
     settings = """
 Please choose the preferred format and video quality for your video. These settings only **apply to YouTube videos**.
